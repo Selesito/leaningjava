e41729a719a507661b0a837e566deb785ad833ff
Selesito <selesitomorozov@gmail.com>
2019-12-27 21:59:16 +0400

    5. Отладка программы

--- chapter_001/src/test/java/leaningjava/condition/SqMaxTest.java
+++ chapter_001/src/test/java/leaningjava/condition/SqMaxTest.java
@@ -10,15 +10,33 @@ import static org.junit.Assert.*;
 public class SqMaxTest {
     @Test
     public void whenp4k1s1() {
-        int expected = 5;
-        int s = SqMax.max(1, 5, 3, 4);
+        int expected = 6;
+        int s = SqMax.max(1, 6, 1, 5);
         assertThat(s, is(expected));
     }
 
     @Test
     public void whenp6k2s2() {
+        int expected = 8;
+        int s = SqMax.max(6, 7, 5, 8);
+        assertThat(s, is(expected));
+    }
+    @Test
+    public void whenp6k2s22() {
         int expected = 6;
-        int s = SqMax.max(6, 2, 5, 4);
+        int s = SqMax.max(5, 4, 6, 1);
+        assertThat(s, is(expected));
+    }
+    @Test
+    public void whenp6k2s222() {
+        int expected = 9;
+        int s = SqMax.max(1, 9, 8, 1);
+        assertThat(s, is(expected));
+    }
+    @Test
+    public void whenp6k2s2222() {
+        int expected = 6;
+        int s = SqMax.max(6, 6, 6, 1);
         assertThat(s, is(expected));
     }
 }

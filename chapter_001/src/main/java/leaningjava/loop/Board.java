a7e1ad0c58d2f41a5b7587af262a79db52df0e4e
Selesito <selesitomorozov@gamil.com>
2019-12-29 23:14:54 +0400

    3. Построить шахматную доску

--- chapter_001/src/main/java/leaningjava/loop/Board.java
+++ chapter_001/src/main/java/leaningjava/loop/Board.java
@@ -3,9 +3,11 @@ public class Board {
     public static void paint(int width, int height) {
         for (int row = 0; row < height; row++) {
             for (int cell = 0; cell < width; cell++) {
-                // условие проверки, что писать пробел или X
+
+                 // условие проверки, что писать пробел или X
                 // Выше в задании мы определили закономерность, когда нужно проставлять X
-                if (cell % 2 == 0) {
+                int result = cell + row;
+                if (result % 2 == 0) {
                     System.out.print("X");
                 } else {
                     System.out.print(" ");

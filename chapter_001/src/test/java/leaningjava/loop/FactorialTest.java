00f00f1d6cf5206b7b807a27a531f12140acfd7a
Selesito <selesitomorozov@gamil.com>
2019-12-29 20:36:05 +0400

    2. Создать программу вычисляющую факториал

---
+++ chapter_001/src/test/java/leaningjava/loop/FactorialTest.java
@@ -0,0 +1,17 @@
+package leaningjava.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FactorialTest {
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
+        int result = Factorial.calc(5);
+        assertThat (result, is(120));
+        //напишите здесь тест, проверяющий, что факториал для числа 5 равен 120.
+    }
+
+
+}

a6eae5ba68802dcdcf2f83ba33ae4fc388c8720c
Selesito <selesitomorozov@gmail.com>
2019-12-30 21:36:11 +0400

    works at a home

---
+++ chapter_001/src/main/java/learningjava/condition/SqArea.java
@@ -0,0 +1,11 @@
+package learningjava.condition;
+
+public class SqArea {
+    public static int square(int p, int k) {
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
+        return s;
+    }
+}
+

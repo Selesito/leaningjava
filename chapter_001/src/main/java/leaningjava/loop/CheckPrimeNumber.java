7986872a525b61024bd9c7d3a37ee9e40f36970e
Selesito <selesitomorozov@gmail.com>
2019-12-31 19:33:27 +0400

    7. Ипотека

--- chapter_001/src/main/java/leaningjava/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/leaningjava/loop/CheckPrimeNumber.java
@@ -2,12 +2,12 @@ package leaningjava.loop;
 
 public class CheckPrimeNumber {
     public boolean check(int finish) {
-        boolean prime = false;
+        boolean prime = true;
         for ( int i = 2; i < finish;i++) {
            if (finish % i == 0) {
                prime = false;
                break;
-           }else prime=true;
+           }
 
         }
         return prime;
--- chapter_001/src/main/java/leaningjava/loop/Mortgage.java
+++ chapter_001/src/main/java/leaningjava/loop/Mortgage.java
@@ -3,16 +3,11 @@ package leaningjava.loop;
 public class Mortgage {
     public int year(int amount, int salary, double percent) {
         int year = 0;
-        double credit=0;
-        double sum = 0;
-        double remains=amount;
-        double percentСredit =  (percent/100);
-        while (true) {
-            credit=  (remains + (remains * percentСredit));
-            sum = credit - salary;
-            remains = sum;
+         while (true) {
+            amount= (int) (amount + (amount * (percent/100)));
+            amount = amount - salary;
             year++;
-            if (remains < 0)break;
+            if (amount < 0)break;
         }
         return year;
     }

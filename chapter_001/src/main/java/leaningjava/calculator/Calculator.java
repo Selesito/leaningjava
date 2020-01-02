e6a67c44504a540d40f9f8b9c516384e7b1c518e
Selesito <selesitomorozov@gamil.com>
2020-01-02 10:11:22 +0400

    8. JavaDoc

--- chapter_001/src/main/java/leaningjava/calculator/Calculator.java
+++ chapter_001/src/main/java/leaningjava/calculator/Calculator.java
@@ -1,5 +1,11 @@
 package leaningjava.calculator;
 
+/**
+ * Calculator.
+ *
+ * @author Selesito (selesitomorozov@yandex.ru)
+ */
+
 public class Calculator {
     public static void add(double first, double second) {
         double result = first + second;
@@ -21,6 +27,10 @@ public class Calculator {
         System.out.println(first + " - " + second + " = " + result);
     }
 
+    /**
+     * Main.
+     * @param args - args
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
---
+++ chapter_001/src/main/java/leaningjava/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Selesito (selesitomorozov@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+package leaningjava.calculator;

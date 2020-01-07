e03bea5417c53912aa04c6a305881b2d2e80474b
Selesito <selesitomorozov@gmail.com>
2020-01-07 14:41:10 +0400

    4. Массив заполнен true или false

---
+++ chapter_001/src/main/java/leaningjava/array/Check.java
@@ -0,0 +1,16 @@
+package leaningjava.array;
+
+public class Check {
+    public boolean mono(boolean[] data) {
+        boolean result = true;
+        for (int index = 1; index!=data.length;index++) {
+            if (data[0]!=data[index]) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+}
---
+++ chapter_001/src/test/java/leaningjava/array/CheckTest.java
@@ -0,0 +1,30 @@
+package leaningjava.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CheckTest {
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, false, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+    @Test
+    public void whenDataNotMonoByTrueThenFalse2() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true, true, true, false};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+}

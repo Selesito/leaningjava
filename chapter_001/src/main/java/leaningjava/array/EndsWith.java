7a19b39113de04dfb8c8d1130a5052e9029c99ab
Selesito <selesitomorozov@gmail.com>
2020-01-07 21:38:41 +0400

    6. Слово заканчивается с...

--- chapter_001/src/main/java/leaningjava/array/EndsWith.java
+++ chapter_001/src/main/java/leaningjava/array/EndsWith.java
@@ -3,16 +3,11 @@ package leaningjava.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        int count = 0;
-        for (int index = post.length-1; index >=0;index--) {
-
-            if (post[index]!=word[word.length - 1 - count]) {
+        for (int index = 0; index != post.length;index++) {
+            if (post[post.length - 1 - index]!=word[word.length - 1 - index ]) {
                 result = false;
                 break;
             }
-            count++;
-
-
         }
         return result;
     }

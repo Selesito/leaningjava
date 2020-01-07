7669e1c3877f420adf5c34a00336f7325be00628
Selesito <selesitomorozov@gmail.com>
2020-01-07 12:25:31 +0400

    5. Простое число

--- chapter_001/src/main/java/leaningjava/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/leaningjava/loop/CheckPrimeNumber.java
@@ -1,14 +1,13 @@
 package leaningjava.loop;
 
 public class CheckPrimeNumber {
-    public boolean check(int finish) {
+    public static boolean check(int finish) {
         boolean prime = true;
         for ( int i = 2; i < finish;i++) {
            if (finish % i == 0) {
                prime = false;
                break;
            }
-
         }
         return prime;
     }

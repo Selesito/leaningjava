6734332976ee0e9451b28fcef750c94e0ea83afe
Selesito <selesitomorozov@gmail.com>
2020-01-07 12:25:53 +0400

    6. Простые числа

--- chapter_001/src/main/java/leaningjava/loop/PrimeNumber.java
+++ chapter_001/src/main/java/leaningjava/loop/PrimeNumber.java
@@ -5,18 +5,9 @@ public class PrimeNumber {
         int number=0;
 
         for (int i = 2; i <= finish; i++) {
-            int count = 0;
-            for (int x = 1; x <= finish; x++) {
-
-                if (i % x == 0){
-                    count++;
-                }
-
-            }
-            if (count==2){
+            if (CheckPrimeNumber.check(i)) {
                 number++;
             }
-
         }
         return number;
     }

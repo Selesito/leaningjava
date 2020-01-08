e12697d7d4cf76aa6b2e1b727572fa86528264b1
Selesito <selesitomorozov@gmail.com>
2020-01-08 14:31:30 +0400

    11. Двумерный массив. Таблица умножения

---
+++ chapter_001/src/main/java/leaningjava/array/Matrix.java
@@ -0,0 +1,15 @@
+package leaningjava.array;
+
+public class Matrix {
+    public int[][] multiple(int size) {
+        int[][] table = new int[size][size];
+        for (int  i = 0; i!=table.length;i++) {
+            for (int j = 0; j!=table.length; j++ ) {
+                table[i][j] = (i+1)*(j+1);
+
+            }
+        }
+        return table;
+    }
+}
+

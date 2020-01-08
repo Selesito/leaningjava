f513dab38fa1aa90c0183e9ad076aa485229041b
Selesito <selesitomorozov@gmail.com>
2020-01-08 17:09:16 +0400

    10. Сортировка выборкой

--- chapter_001/src/main/java/leaningjava/array/SortSelected.java
+++ chapter_001/src/main/java/leaningjava/array/SortSelected.java
@@ -16,12 +16,3 @@ public class SortSelected {
 
 }
 
-//        for(int i = data.length-1 ; i > 0 ; i--){
-//        for(int j = 0 ; j < i ; j++) {
-//
-//        if( data[j] > data[j+1] ){
-//        int tmp = data[j];
-//        data[j] = data[j+1];
-//        data[j+1] = tmp;
-//        }
-//        }

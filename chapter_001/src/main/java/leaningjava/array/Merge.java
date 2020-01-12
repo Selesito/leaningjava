40e42f0ebb2b4feb7d53fc057de44403a00acd24
Selesito <selesitomorozov@gmail.com>
2020-01-12 19:46:10 +0400

    1. Объеденить два массива

--- chapter_001/src/main/java/leaningjava/array/Merge.java
+++ chapter_001/src/main/java/leaningjava/array/Merge.java
@@ -5,11 +5,8 @@ import java.util.Arrays;
 public class Merge {
     public int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-
         int i = 0, j = 0, index = 0;
-
         while (index < rsl.length) {
-
             if (i > left.length - 1) {
                 rsl[index] = right[j];
                 j++;

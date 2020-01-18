0444fb3280f9b2018d275bfb3be1568d2c096408
Selesito <selesitomorozov@gmail.com>
2020-01-18 21:27:13 +0400

    5. Поля объекта

--- chapter_001/src/main/java/leaningjava/oop/Cat.java
+++ chapter_001/src/main/java/leaningjava/oop/Cat.java
@@ -1,9 +1,32 @@
 package leaningjava.oop;
 
 public class Cat {
-    public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.name + " - " + this.food);
     }
 
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Barsik");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("Ryzhik");
+        black.eat("fish");
+        black.show();
+    }
 }

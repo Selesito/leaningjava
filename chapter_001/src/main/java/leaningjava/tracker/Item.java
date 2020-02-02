148460f1b77ad37989a0e858e69b2f60820a0fda
Selesito <selesitomorozov@gmail.com>
2020-02-02 20:51:24 +0400

    0.1 Код к заданию.

---
+++ chapter_001/src/main/java/leaningjava/tracker/Item.java
@@ -0,0 +1,14 @@
+package leaningjava.tracker;
+
+public class Item {
+    public Item() {
+        super();
+        System.out.println("load item");
+    }
+
+    public static void main(String[] args) {
+        Bug bug = new Bug();
+    }
+
+}
+

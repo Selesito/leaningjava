a39649fc609bb93f8af0d4d352e0918a07676d19
Selesito <selesitomorozov@gmail.com>
2020-02-04 23:45:28 +0400

    5. Удаление моделей из массива

---
+++ chapter_001/src/main/java/leaningjava/pojo/Shop.java
@@ -0,0 +1,70 @@
+package leaningjava.pojo;
+
+public class Shop {
+    public static void main(String[] args) {
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Удаляем значение из ячейки с индексом 1");
+        products[1] = null;
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Записываем  в ячейку с индексом 1 значение ячейки с индексом 2 и удаляем значение из ячейки с индексом 2");
+        products[1] = products[2];
+        products[2] = null;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        Shop shop = new Shop();
+        shop.delete(products, 0);
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+
+    }
+    public Product[] delete(Product[] products, int index) {
+        products[index] = null;
+        for (int i = index; i < products.length; i++) {
+            if (i == products.length - 1) {
+                break;
+            }
+            products[i] = products[i + 1];
+        }
+        return products;
+    }
+
+
+}

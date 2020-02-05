2904cd3c2750f17b5e5e18aac0153a61c4d6d031
Selesito <selesitomorozov@gmail.com>
2020-02-05 20:43:25 +0400

    4. Массивы и модели

--- chapter_001/src/main/java/leaningjava/pojo/Shop.java
+++ chapter_001/src/main/java/leaningjava/pojo/Shop.java
@@ -6,6 +6,8 @@ public class Shop {
         products[0] = new Product("Milk", 10);
         products[1] = new Product("Bread", 4);
         products[2] = new Product("Egg", 19);
+        products[3] = new Product("Eggg", 119);
+//        products[4] = new Product("Egggg", 129);
 
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
@@ -17,33 +19,33 @@ public class Shop {
         }
 
         System.out.println();
-        System.out.println("Удаляем значение из ячейки с индексом 1");
-        products[1] = null;
+//        System.out.println("Удаляем значение из ячейки с индексом 1");
+//        products[1] = null;
+//
+//        for (int i = 0; i < products.length; i++) {
+//            Product product = products[i];
+//            if (product != null) {
+//                System.out.println(product.getName());
+//            } else {
+//                System.out.println("null");
+//            }
+//        }
 
-        for (int i = 0; i < products.length; i++) {
-            Product product = products[i];
-            if (product != null) {
-                System.out.println(product.getName());
-            } else {
-                System.out.println("null");
-            }
-        }
-
-        System.out.println();
-        System.out.println("Записываем  в ячейку с индексом 1 значение ячейки с индексом 2 и удаляем значение из ячейки с индексом 2");
-        products[1] = products[2];
-        products[2] = null;
-        for (int i = 0; i < products.length; i++) {
-            Product product = products[i];
-            if (product != null) {
-                System.out.println(product.getName());
-            } else {
-                System.out.println("null");
-            }
-        }
+//        System.out.println();
+//        System.out.println("Записываем  в ячейку с индексом 1 значение ячейки с индексом 2 и удаляем значение из ячейки с индексом 2");
+//        products[1] = products[2];
+//        products[2] = null;
+//        for (int i = 0; i < products.length; i++) {
+//            Product product = products[i];
+//            if (product != null) {
+//                System.out.println(product.getName());
+//            } else {
+//                System.out.println("null");
+//            }
+//        }
 
         Shop shop = new Shop();
-        shop.delete(products, 0);
+        shop.delete(products, 1);
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
             if (product != null) {
@@ -59,6 +61,7 @@ public class Shop {
         products[index] = null;
         for (int i = index; i < products.length; i++) {
             if (i == products.length - 1) {
+                products[i] = null;
                 break;
             }
             products[i] = products[i + 1];

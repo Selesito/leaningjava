391e0939cf02b6c8fe6155f975ef2a3935ab0bc8
Selesito <selesitomorozov@gmail.com>
2020-02-17 22:27:36 +0400

    TrackerTest edit

--- chapter_001/src/test/java/leaningjava/tracker/TrackerTest.java
+++ chapter_001/src/test/java/leaningjava/tracker/TrackerTest.java
@@ -14,22 +14,6 @@ public class TrackerTest {
         assertThat(result.getName(), is(item.getName()));
     }
 
-    @Test
-    public void whenReplaceNameThenReturnNewName() {
-        Tracker tracker = new Tracker();
-        Item previous = new Item("test1");
-        // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
-        tracker.add(previous);
-        // Создаем новую заявку.
-        Item next = new Item("test2");
-        // Проставляем старый id из previous, который был сгенерирован выше.
-        next.setId(previous.getId());
-        // Обновляем заявку в трекере.
-        tracker.replace("previous.getId()", next);
-        // Проверяем, что заявка с таким id имеет новые имя test2.
-        assertThat(tracker.findById(previous.getId()).getName(), is("test2"));
-    }
-
 
     @Test
     public void whenDeleteNameThenReturnBooleanResult() {

028d0a440bf08185d0c5c99e35107f432e5d1038
Selesito <selesitomorozov@gmail.com>
2020-05-23 10:03:29 +0400

    Знакомство с коллекции java.util.HashSet.

---
+++ chapter_003/src/main/java/leaningjava/collection/UsageHashSet.java
@@ -0,0 +1,17 @@
+package leaningjava.collection;
+
+import java.util.HashSet;
+
+public class UsageHashSet {
+    public static void main(String[] args) {
+        HashSet<String> autos = new HashSet<String>();
+        autos.add("Lada");
+        autos.add("BMW");
+        autos.add("Volvo");
+        autos.add("Toyota");
+        autos.add("BMW");
+        for (String auto : autos) {
+            System.out.println(auto);
+        }
+    }
+}

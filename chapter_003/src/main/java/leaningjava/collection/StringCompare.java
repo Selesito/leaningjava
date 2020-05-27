22af1ba38a4bc2474f687cd8439b4989dd115d67
Selesito <selesitomorozov@gamil.com>
2020-05-27 23:20:57 +0400

    3. Компаратор для строк. [#191329]

---
+++ chapter_003/src/main/java/leaningjava/collection/StringCompare.java
@@ -0,0 +1,21 @@
+package leaningjava.collection;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+           int result = 0;
+           int count = Math.min(left.length(), right.length());
+           for (int index = 0; index < count; index++ ){
+                   result = Character.compare(left.charAt(index), right.charAt(index));
+               if (result != 0) {
+                   break;
+               }
+           }
+           if (result == 0) {
+               result = Integer.compare(left.length(), right.length());
+           }
+           return result;
+    }
+}
